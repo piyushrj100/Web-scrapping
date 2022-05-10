14ba7ef2917b62bcaa2672c47dd4deef35764b53
Piyush Raja <piyush1.bksc@gmail.com>
2022-05-10 22:46:42 +0530

    added functionality for json

--- webscrap_practicl/main.py
+++ webscrap_practicl/main.py
@@ -1,18 +1,20 @@
 from bs4 import BeautifulSoup
 import requests
 import time
+import json
 
 print('Put some skill that you are not familiar with')
 unfamiliar_skill = input('>')
 print(f'Filtering out {unfamiliar_skill}')
 
 def find_jobs() :
+    description ={'job_description' : [] } #to create array of json obj
     html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=Python&txtLocation=India').text
     # print(html_text)
     soup = BeautifulSoup(html_text, 'html.parser')
     job_cards = soup.find_all('li', class_ = 'clearfix job-bx wht-shd-bx')
-    for index,job in enumerate(job_cards) : 
-        published_date = job.find('span', class_ = 'sim-posted').span.text
+    for job in job_cards : 
+        published_date = job.find('span', class_ = 'sim-posted').span.text.strip()
 
         # job.find('i', class_ = 'material-icons').extract() #for removing the materials icon tag for first li
         # job.find('i', class_ = 'material-icons').extract() #for removing the second materials icon tag
@@ -25,20 +27,26 @@ def find_jobs() :
         if location is None :
             location=job_loc_exp.select_one(":nth-child(3)").span
         job_location = location.text
-        print(job_location, experience)
         
-        # if 'few'  not in published_date:
-        #     company_name = job.find('h3', class_ = 'joblist-comp-name' ).text.replace(' ', '')
-        #     skills = job.find('span', class_ = 'srp-skills').text.replace(' ', '')
-        #     more_info = job.header.h2.a['href']
-        #     if unfamiliar_skill not in skills :
-        #         with open(f'post/{index}.txt','w') as f :
-        #             f.write(f'Company Name : {company_name.strip()}\n')
-        #             f.write(f'Required Skills : {skills.strip()}\n')
-        #             f.write(f'Post made : {published_date.strip()}\n')
-        #             f.write(f'Link : {more_info.strip()}\n')
-        #             f.write()
-        #         print('File Saved')
+        if 'few'  not in published_date:
+            company_name = job.find('h3', class_ = 'joblist-comp-name' ).text.replace(' ', '').strip()
+            skills = job.find('span', class_ = 'srp-skills').text.replace(' ', '').strip()
+            more_info = job.header.h2.a['href'].strip()
+            if unfamiliar_skill not in skills :
+                job_info = {'Company Name' : company_name,
+                            'Experience Required' : experience,
+                            'Location' : job_location,
+                            'Key Skills' : skills,
+                            'More Info' : more_info,
+                            'Publishing Date' : published_date
+                
+                }
+                description['job_description'].append(job_info)
+    timestamp = int(time.time())
+
+    #writing to the json file created.
+    with open(f'post/file_{timestamp}.json','w') as f :
+        json.dump(description,f, indent = 4, sort_keys=True)
                 
 
 if __name__ == '__main__' :
